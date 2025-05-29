77c6d939b770dabedf7d91b7212be6ad6522d829
SamruddhiAmbre <sam.ambre2211@gmail.com>
2025-05-29 22:27:15 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Weather-App/', // ← ADD THIS LINE
+  base: '/Weather-App/', 
   plugins: [react()],
 });
